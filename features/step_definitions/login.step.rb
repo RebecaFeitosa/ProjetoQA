6dd5c5f91f7503d300ef388a1260afa56a21ff3b
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2025-01-02 18:24:57 -0300

    steps dos testes

---
+++ features/step_definitions/login.step.rb
@@ -0,0 +1,13 @@
+# Escrevi o bdd (na pasta de bdd no arquivo de login), depois dei o comando 'cucumber' no terminal para gerar as chaves automáticas dos steps e colei elas abaixo
+Dado('que loguei') do
+    visit ''
+    sleep 5
+    end
+  
+  Quando('eu Logar') do
+
+    end
+  
+  Entao('logaremos') do
+  
+    end
