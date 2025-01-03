c26f88d0729e86de633c6fa1d1bf6d80f26a77e3
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2025-01-03 18:56:18 -0300

    att

--- features/support/hoocks.rb
+++ features/support/hoocks.rb
@@ -1,5 +1,6 @@
-# Maximizar a página antes de começar o teste e enviar a mensagem de teste finalizado quando acabar o teste
+# O hoocks é um recurso que executa comandos especificos antes da automação iniciar e depois da automação terminar
 
+# Maximizar a página antes de começar o teste e enviar a mensagem de teste finalizado quando acabar o teste
 Before do
     Capybara.page.driver.browser.manage.window.maximize
 end
