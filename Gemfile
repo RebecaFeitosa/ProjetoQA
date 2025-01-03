0152cbc26f0aed1a951cd156710ba57f10a233ac
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2025-01-03 17:32:13 -0300

    config

--- Gemfile
+++ Gemfile
@@ -1,15 +1,14 @@
-# No site indicado, pesquisei 'bundler' que é uma gem que serve para gerenciar as gems dentro do projeto, copiei o arquivo de instalação e colei no terminal
 # Indicar onde o Ruby deve buscar as Gems do projeto
 source "http://rubygems.org"
 
-# Instrução para que o Ruby consiga baixar e utilizar a gem (pesquisar a gem e pegar o gemfile no site)
+# Instrução para que o Ruby consiga baixar e utilizar a gem
 gem 'cucumber', '~> 9.2'
 gem 'capybara', '~> 3.40'
 gem 'selenium-webdriver', '~> 4.27'
 gem 'rspec', '~> 3.13'
 gem 'site_prism', '~> 5.0', '>= 5.0.4'
-# Após pegar os arquivos das gems, rodar o comando 'bundler install' e vai ser instalado todas as gems
+# Após pegar os arquivos das gems, rodar o comando 'bundler install'
 
 # Rodar o comando 'cucumber --init'
-# Serão criadas pastas e arquivos, dentro da pasta 'support', no arquivo 'env.rb' é necessário importar as gems para o código (próximos passos lá)
+# Serão criadas pastas e arquivos, dentro da pasta 'support', no arquivo 'env.rb' é necessário importar as gems para o código
 # Sempre importar as gems no arquivo 'env.rb'
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,11 +5,15 @@ require 'site_prism'
 require 'site_prism/all_there'
 require 'rspec'
 require 'rspec/expectations'
-include RSpec::Matchers     # Isso permite que você use as expressões de comparação e verificação do RSpec em qualquer lugar do projeto
+require_relative 'page.initialize.rb'
+include RSpec::Matchers
 
-# Inicializando as configurações padrões do capybara e relacionando a variável config as configurações do capybara
+Environment = ENV['URL']
+
+World(Page)
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome                # Indicação de que estou usando o selenium com o chrome como driver principal
-    config.app_host = 'https://www.linkedin.com/login/'     # A página que ele deve visitar para realizar os testes, como coloquei aqui como padrão, não preciso colocar essa url nos testes, apenas aspas vazias no visit '' (Coloquei o site do linkedin/login, pois na página principal estava dando erro)
-    config.default_max_wait_time = 5                        # Setando o tempo máximo que o capybara deve esperar por um elemento na tela
+    config.default_driver = :selenium_chrome        
+    config.app_host = Environment                    
+    config.default_max_wait_time = 5                
 end
